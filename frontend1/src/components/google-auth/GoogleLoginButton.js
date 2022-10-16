d32862720cc061a8cfa327b6efff1f902f2ae92f
Yi Zu <zuyi@bu.edu>
2022-10-16 02:33:23 -0400

    Mock do login to dashboard

--- frontend1/src/components/google-auth/GoogleLoginButton.js
+++ frontend1/src/components/google-auth/GoogleLoginButton.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
+import Router from 'next/router';
 
 const client_id = "247747261514-8k1qikhl0urh29oema81jeuod9g5kc37.apps.googleusercontent.com";
 
 function GoogleLoginButton() {
     const onSuccess = (res) => {
         console.log("Successful login! Current user: ", res.profileObj);
+
+        setTimeout(() => {
+         Router
+        .push('/')
+        .catch(console.error);
+        }, 4500);
+
     }
 
     const onFailure = (res) => {
